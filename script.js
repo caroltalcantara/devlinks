31a6a739d77b90094a15bf8dd5ac1055cda98da8
Caroline <carolalcantara367@gmail.com>
2023-08-19 11:37:08 -0300

    add script

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMode() {
   // substituir a img
   if (html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute('scr', './assets/avatar-light.png')
+    img.setAttribute('src', './assets/avatar-light.png')
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute('src', './assets/avatar.png')
