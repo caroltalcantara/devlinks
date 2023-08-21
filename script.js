bea8bc1b4ee9b8bbb6388b1209eb1db9601d3afb
Caroline <carolalcantara367@gmail.com>
2023-08-21 16:19:58 -0300

    add changes

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   // substituir a img
   if (html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', './assets/carol-light.jpg')
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src', './assets/carol-dark.jpg')
   }
 }
